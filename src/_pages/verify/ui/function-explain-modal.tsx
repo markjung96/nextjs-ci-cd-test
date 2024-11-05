e71df1e8169ad235c44b292776f0922c66df3714
Mark_Jeong <81726531+markjung96@users.noreply.github.com>
2024-11-05 15:33:00 +0900

    feat: scroll modal

--- src/_pages/verify/ui/function-explain-modal.tsx
+++ src/_pages/verify/ui/function-explain-modal.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
   Dialog,
   DialogContent,
@@ -8,35 +8,30 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
-import { Button } from "@/src/shared/ui";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import {
-  a11yDark,
-  a11yLight,
-} from "react-syntax-highlighter/dist/esm/styles/hljs";
-import { MarkdownPreview } from "./mark-down-preview";
-import { useTheme } from "next-themes";
+} from '@/components/ui/dialog';
+import { Button } from '@/src/shared/ui';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { a11yDark, a11yLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { MarkdownPreview } from './mark-down-preview';
+import { useTheme } from 'next-themes';
 
 interface FunctionExplainModalProps {
   code?: string;
 }
 
-export default function FunctionExplainModal({
-  code,
-}: FunctionExplainModalProps) {
+export default function FunctionExplainModal({ code }: FunctionExplainModalProps) {
   const { theme } = useTheme();
   const [isOpen, setIsOpen] = useState(false);
-  const [explanation, setExplanation] = useState("");
+  const [explanation, setExplanation] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const getExplanationByApi = async (code: string) => {
     setIsLoading(true);
-    const result = await fetch("api/chat", {
-      method: "POST",
+    const result = await fetch('api/chat', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ content: code }),
     });
@@ -65,33 +60,31 @@ export default function FunctionExplainModal({
           <DialogTitle>Function code and explanation</DialogTitle>
         </DialogHeader>
         <div className="flex flex-col gap-4 mt-4 ">
-          {/* 왼쪽: 코드 */}
-          <div className="max-h-[50%]">
+          <div>
             <h3 className="text-lg font-semibold mb-2">Code</h3>
             <pre className="bg-white-800 dark:bg-gray-800 text-white rounded overflow-x-auto">
-              <ScrollArea className="max-h-[calc(100%-2rem)] rounded border p-4">
+              <div className="rounded border p-4 max-h-[calc(50vh-92px)] overflow-y-scroll">
                 <SyntaxHighlighter
                   language="solidity"
                   wrapLongLines
                   className="text-sm"
-                  style={theme === "dark" ? a11yDark : a11yLight}
+                  style={theme === 'dark' ? a11yDark : a11yLight}
                 >
-                  {code || ""}
+                  {code || ''}
                 </SyntaxHighlighter>
-              </ScrollArea>
+              </div>
             </pre>
           </div>
-          {/* 오른쪽: 설명 */}
           {isLoading ? (
             <div className="flex-1 flex items-center justify-center">
               <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
             </div>
           ) : (
-            <div className="max-h-[50%]">
+            <div>
               <h3 className="text-lg font-semibold mb-2">Explanation</h3>
-              <ScrollArea className="h-[calc(100%-2rem)] rounded border p-4 flex-1">
-                <MarkdownPreview markdown={explanation} />
-              </ScrollArea>
+              <div className="rounded border p-4 max-h-[calc(50vh-92px)] overflow-y-scroll">
+                <MarkdownPreview markdown={explanation + explanation} />
+              </div>
             </div>
           )}
         </div>
