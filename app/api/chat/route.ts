6a49eeb84d2156953cf8e58592a7f3ea906231b0
DoohyunCho <doo.cho.star@gmail.com>
2024-11-16 20:38:59 +0700

    fix: ai 모델 수정

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: Request) {
   const { content } = await req.json();
   const prompt = `Explain the code below in plain English: \n\n${content}`;
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' });
+  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash-8b' });
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
